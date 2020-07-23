987cb479a020e81fa798ba006aa89e8e782e0f36
Gabriela Ramos <gabriela.ramos@epn.edu.ec>
2020-07-22 22:53:10 -0500

    http module clase 9

--- 01-http/introduccion/src/http/http.module.ts
+++ 01-http/introduccion/src/http/http.module.ts
@@ -1,3 +1,12 @@
+import {Module} from '@nestjs/common';
+
+//@ --> decorador : son para poder decir que clase son que cosa
+@Module({
+        imports: [],
+        controllers:[],
+        providers:[],
+    })
+
 export class HttpModule {
 
 }
