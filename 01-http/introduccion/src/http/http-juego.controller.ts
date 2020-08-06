9be93ce5ccd410995058e3bcab9e690ec53be765
Gabriela Ramos <gabriela.ramos@epn.edu.ec>
2020-08-05 21:18:01 -0500

    Proyecto I bimestre

--- 01-http/introduccion/src/http/http-juego.controller.ts
+++ 01-http/introduccion/src/http/http-juego.controller.ts
@@ -1,4 +1,20 @@
-import {BadRequestException, Controller, Delete, Get, Header, HttpCode, Param, Post} from "@nestjs/common";
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Header,
+    HttpCode,
+    Param,
+    Post,
+    Query,
+    Req,
+    Res,
+    Headers
+} from "@nestjs/common";
+import {MascotaCreateDto} from "./dto/mascota.create-dto";
+import {validate, ValidationError} from "class-validator";
 // http://localhost:3001/juegos-http
 //juego-http --> segmento de red
 //tenemos definido la IP, el puerto y el la parte del url --falta el metodo
@@ -38,30 +54,134 @@ ParametrosRutaEjemplo(
 ){
 console.log("Parametros", parametrosRuta);
 //validar si es un numero
-    let altura=0
-    let edad=0
-
-   if(isNaN(parametrosRuta.edad) || isNaN(parametrosRuta.altura))
-       throw new BadRequestException("No son numeros")
-   else
-       edad= Number(parametrosRuta.edad);
-        altura= Number(parametrosRuta.altura);
-
-    return edad+altura;
-
+    isNaN(parametrosRuta.edad) // 'ABC' true
+    isNaN(parametrosRuta.altura) //  1234 false
+    // throw new BadRequestException('No son numeros')
+    const edad = Number(parametrosRuta.edad);
+    const altura = Number(parametrosRuta.altura);
+    return edad + altura;
 }
+
     @Get('parametros-consulta')
     parametrosConsulta(
         @Query() parametrosDeConsulta //lo que quiero recibir @Query es 
     ) {
-        console.log(parametrosDeConsulta.nombre);
-        console.log(parametrosDeConsulta.apellido);
-        const tieneNombreYApellido = parametrosDeConsulta.nombre != undefined && parametrosDeConsulta.apellido != undefined;
+        console.log(parametrosDeConsulta.a);
+        console.log(parametrosDeConsulta.b);
+        const tieneNombreYApellido = parametrosDeConsulta.a != undefined && parametrosDeConsulta.b != undefined;
         console.log('parametrosDeConsulta', parametrosDeConsulta);
         if (tieneNombreYApellido) {
-            return parametrosDeConsulta.nombre + ' ' + parametrosDeConsulta.apellido;
+            return parametrosDeConsulta.a + ' ' + parametrosDeConsulta.b;
         } else {
             return '= )';
         }
     }
+
+    @Post('parametros-cuerpo')
+    @HttpCode(200)
+    async parametrosCuerpo(
+        @Body() parametrosDeCuerpo
+    ) {
+        // Promesas
+        const mascotaValida = new MascotaCreateDto();
+        mascotaValida.casada = parametrosDeCuerpo.casada;
+        mascotaValida.edad = parametrosDeCuerpo.edad;
+        mascotaValida.ligada = parametrosDeCuerpo.ligada;
+        mascotaValida.nombre = parametrosDeCuerpo.nombre;
+        mascotaValida.peso = parametrosDeCuerpo.peso;
+        try {
+            const errores: ValidationError[] = await validate(mascotaValida);
+            if (errores.length > 0) {
+                console.error('Errores: ', errores);
+                throw new BadRequestException('Error validando');
+            } else {
+                const mensajeCorrecto = {
+                    mensaje: 'Se creo correctamente'
+                };
+                return mensajeCorrecto;
+            }
+        } catch (e) {
+            console.error('Error', e);
+            throw new BadRequestException('Error validando');
+        }
+    }
+    @Get('guardarCookieInsegura')
+    guardarCookieInsegura(
+        @Query() parametrosConsulta,
+        @Req() req, //  request - PETICION
+        @Res() res // response - RESPUESTA
+    ) {
+        res.cookie(
+            'galletaInsegura', // nombre
+            'Tengo hambre', // valor
+        );
+        const mensaje = {
+            mensaje: 'ok'
+        };
+        // return mensaje; // NO SE PUEDE USAR RETURN CUANDO SE USA @Res() OJO !!!
+        res.send(mensaje); // METODO EXPRESSJS
+    }
+
+    @Get('guardarCookieSegura')
+    guardarCookieSegura(
+        @Query() parametrosConsulta,
+        @Req() req, //  request - PETICION
+        @Res() res // response - RESPUESTA
+    ) {
+        res.cookie(
+            'galletaSegura', // nombre
+            'Web :3', // valor
+            {
+                secure: true
+            }
+        );
+        const mensaje = {
+            mensaje: 'ok'
+        };
+        // return mensaje; // NO SE PUEDE USAR RETURN CUANDO SE USA @Res() OJO !!!
+        res.send(mensaje); // METODO EXPRESSJS
+    }
+
+    @Get('mostrarCookies')
+    mostrarCookies(
+        @Req() req
+    ) {
+        const mensaje = {
+            sinFirmar: req.cookies,
+            firmadas: req.signedCookies
+        };
+        return mensaje;
+    }
+
+    @Get('guardarCookieFirmada')
+    public guardarCookieFirmada(
+        @Res() res,
+        @Headers() headers // peticion - request
+    ) {
+        // ENCRIPCION DE LA POLIBURGUER CON EL ALGORITMO Q YO QUIERO
+        console.log('Headers', headers);
+
+        res.header('Cabecera','Dinamica'); // respuesta - response
+
+        res.cookie('firmada', 'poliburguer', {signed: true});
+        res.cookie('firmada1', 'poliburguer1', {signed: true});
+        res.cookie('firmada2', 'poliburguer2', {signed: true});
+        res.cookie('firmada3', 'poliburguer3', {signed: true});
+        res.cookie('firmada4', 'poliburguer4', {signed: true});
+
+
+
+
+        const mensaje = {
+            mensaje: 'ok'
+        };
+        res.send(mensaje);
+    }
+
+    // 1 Guardar Cookie Insegura
+    // 2 Guardar Cookie Segura
+    // 3 Mostrar Cookies
+
+
+
 }
